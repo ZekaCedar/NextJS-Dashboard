273538aa7d142318f4eb0b377f44b4706331ab9d
ZekaCedar <zulaikha.affandi@gmail.com>
2024-09-18 15:12:03 +0800

    fix could not delete invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -115,9 +115,9 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 export async function deleteInvoice(id: string) {
 
-  throw new Error('Failed to Delete Invoice');
+  // throw new Error('Failed to Delete Invoice');
  
-  // Unreachable code block
+  // // Unreachable code block
 
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
